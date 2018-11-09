4155df8ef41780e2ee15c6bfaaeb108fb7158da5
Dam H <damhansen1995@gmail.com>
2018-11-09 14:32:58 +0100

    Penguins now dance

---
+++ Assets/Scripts/JumpAtLedge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class JumpAtLedge : MonoBehaviour {
+
+    Animator animator;
+    NavMeshAgent agent;
+    bool jumpOnce = true;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+  
+        playAnime();
+    }
+    private void playAnime()
+    {
+        if (jumpOnce)
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                Debug.Log("Succesfully Jumped");
+                animator.SetTrigger("Jump");
+                jumpOnce = false;
+            }
+        }
+    }
+}
